969f207a34a488e6e2294e393942c0478931e6fd
Onur Turan <turan.onur28@gmail.com>
2017-10-25 15:21:14 +0300

    silnecek adlı controller oluşturuldu.

---
+++ MvcBlogum/MvcBlogPortfolio/Controllers/SilinecekController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBlogPortfolio.Controllers
+{
+    public class SilinecekController : Controller
+    {
+        // GET: Silinecek
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
